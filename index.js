dfad4a95900b3962437917036de95f8ff46ab9db
Za_Genius <rapzter58@gmail.com>
2023-09-08 11:15:52 +0100

    ci: integrate the app with express

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('node:http');
+const express = require('express');
 // const fetch = require('node:fetch');
 // require('dotenv').config()
 
@@ -17,35 +18,32 @@ const http = require('node:http');
 // })
 
 
+const app = express()
 const currentDate = new Date()
-const server = http.createServer((req,res)=>{
-    const reqUrl = new URL(req.url,`http://${req.headers.host}`)
-    const searchParams = {};
-    reqUrl.searchParams.forEach((value,name)=>{
-        searchParams[name] = value;
-    })
-  
-    
-    if(req.method === 'GET' && req.url.startsWith('/api')){
-        const hngInternDetail = {
-            slack_name: searchParams.slack_name ??'slack name here',
-            current_day: currentDate.toLocaleString('en-us', {  weekday: 'long' }),
-            utc_time: currentDate.toISOString().split('.')[0] +'Z',
-            track: searchParams.track ?? "track here",
-            github_file_url: "https://github.com/oluwatobiloba1/Hng-stage1-task/blob/main/index.js",
-            github_repo_url: "https://github.com/oluwatobiloba1/Hng-stage1-task",
-            status_code: 200
-        }
-        res.writeHead(200,{'Content-Type': "application/json"})
-        res.end(JSON.stringify(hngInternDetail));
+const port = 5000
 
-    }
-    else{
-        res.writeHead(404,{'Content-Type': "application/json"})
-        res.end(JSON.stringify({message: "you must be trying to access the '/api' route. Don't forget to do that and include the search params"}));
-    }
+app.get('/api', (req, res) => {
+  const hngInternDetail = {
+    slack_name: req.query.slack_name ??'slack name here',
+    current_day: currentDate.toLocaleString('en-us', {  weekday: 'long' }),
+    utc_time: currentDate.toISOString().split('.')[0] +'Z',
+    track: req.query.track ?? "track here",
+    github_file_url: "https://github.com/oluwatobiloba1/Hng-stage1-task/blob/main/index.js",
+    github_repo_url: "https://github.com/oluwatobiloba1/Hng-stage1-task",
+    status_code: 200
+}
+
+app.get('*', (req, res) => {
+
+  res.end(JSON.stringify({message: "you must be trying to access the '/api' route. Don't forget to do that and include the search params"})).status(404);
 })
 
-server.listen(5000,()=>{
-    console.log('server is running on port 5000');
+
+res.json(hngInternDetail).status(200)
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
 })
